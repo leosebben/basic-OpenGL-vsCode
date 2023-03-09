0cbe266cfff548aae83762b0573eb37b41419e25
leo-sebben <68429302+leosebben@users.noreply.github.com>
2023-03-09 12:45:38 -0300

    basic clear window

--- main.cpp
+++ main.cpp
@@ -1,37 +1,71 @@
+#include <stdio.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-int main(void)
+// window dimensions
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int error(char *message);
+
+int main() 
 {
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    // initialize GLFW
+    if (!glfwInit()) {
+        printf("GLFW initialization failed");
         glfwTerminate();
-        return -1;
+        return 1;
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    // setup glfw window property
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                  // opengl version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // core profile = no compatibility
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // allow forward compatibility
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
+    GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Hello, World!", NULL, NULL);
+    if (!mainWindow) {
+        printf("GLFW window creation failed");
+        glfwTerminate();
+        return 1;
+    }
+
+    // buffer size informatino
+    int bufferWidth, bufferHeight;
+    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+    // set context for GLEW to use
+    glfwMakeContextCurrent(mainWindow);
+
+    // allow modern extension features
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK) {
+        printf("GLEW initialization failed");
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return 1;
+    }
+
+    // setup viewport size
+    glViewport(0, 0, bufferWidth, bufferHeight);
+
+    // loop until window close
+    while(!glfwWindowShouldClose(mainWindow)) {
+        // get and handle user input events
+        glfwPollEvents();
+
+        // clear the window
+        glClearColor(1.0f, 1.0f, 0.8f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
+        glfwSwapBuffers(mainWindow);
     }
 
-    glfwTerminate();
     return 0;
+}
+
+int error(char *message) {
+    printf("%s", message);
+    glfwTerminate();
+    return 1;
 }
