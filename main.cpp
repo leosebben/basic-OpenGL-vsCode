27b30fa742ef3e35fcddc8d542482c00553b28a7
leo-sebben <68429302+leosebben@users.noreply.github.com>
2023-03-09 12:46:29 -0300

    fix minor

--- main.cpp
+++ main.cpp
@@ -17,10 +17,10 @@ int main()
     }
 
     // setup glfw window property
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                  // opengl version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // opengl version
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // core profile = no compatibility
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // allow forward compatibility
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // core profile = no compatibility
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // allow forward compatibility
 
     GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Hello, World!", NULL, NULL);
     if (!mainWindow) {
